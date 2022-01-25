7784b2c1e2316145732bf5bafbdc5ff5111faac6
ullas <ullaskunder3@gmail.com>
2022-01-25 18:09:42 +0530

    ✨Login and register µ

--- routes/auth.js
+++ routes/auth.js
@@ -8,13 +8,31 @@ router.post('/register', async (req, res)=>{
         {
             username: req.body.username,
             email: req.body.email,
-            password: cryptoJs.AES.encrypt(req.body.password, process.env.SECRET_KEY),
+            password: cryptoJs.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString(),
         }
     );
 
     try {
         const savedUser = await newUser.save();
         res.status(201).json(savedUser);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+// login
+router.post('/login', async (req, res)=>{
+    try {
+        const user = await User.findOne({username: req.body.username});
+        !user && res.status(401).json('wrong credientials!')
+
+        const hashPass = cryptoJs.AES.decrypt(user.password, process.env.SECRET_KEY);
+        const userPassword = hashPass.toString(cryptoJs.enc.Utf8);
+        userPassword !== req.body.password && res.status(401).json('wrong credientials!')
+
+        const {password, ...others} = user._doc;
+        res.status(200).json(others);
+        
     } catch (error) {
         res.status(500).json(error);
     }
