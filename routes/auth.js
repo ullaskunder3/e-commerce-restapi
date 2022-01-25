720584b4c992f5a0dbeb572c70a6cf77e4444472
ullas <ullaskunder3@gmail.com>
2022-01-25 17:41:53 +0530

    hidding .env & adding key

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const cryptoJs = require('crypto-js');
 
 // Register
 router.post('/register', async (req, res)=>{
@@ -7,7 +8,7 @@ router.post('/register', async (req, res)=>{
         {
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password,
+            password: cryptoJs.AES.encrypt(req.body.password, process.env.SECRET_KEY),
         }
     );
 
